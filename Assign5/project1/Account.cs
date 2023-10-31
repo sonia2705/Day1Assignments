056ed78a11da97b6259c3f79f3c9466a230eafc7
sonia2705 <65391974+sonia2705@users.noreply.github.com>
2023-10-31 19:50:58 +0530

    Add files via upload

---
+++ Assign5/project1/Account.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace Assign5_s
+{
+    public class Account
+    {
+        int id;
+        public int Id 
+        { get { return id; }
+            set { id = value; }
+        }
+        string accountType;
+        public string AccountType
+        {
+            get { return accountType; }
+            set { accountType = value; }
+        }
+        double balance;
+        public double Balance
+        {
+            get { return balance;}
+            set { balance = value; }    
+        }
+        public void GetDetails()
+        {
+            
+            Console.WriteLine("Enter account id");
+            id = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter account type");
+            accountType = Console.ReadLine();
+            Console.WriteLine("Enter account balance");
+            balance = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter account type");
+            
+        }
+        public bool WithDraw(double amount)
+        {
+            if (amount < balance)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+        public void displaydetails()
+        {
+            Console.WriteLine($"Account ID : {id} \n Account Type : {accountType} \n Balance : {balance} \n");        }
+    }
+}
---
+++ Assign5/project1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assign5_s;
+
+namespace Assign5_s
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account();
+            account.GetDetails();
+            Console.WriteLine("Account details are : \n");
+            account.displaydetails();
+            Console.WriteLine("Enter amount to withdraw : ");
+            double amount = Double.Parse(Console.ReadLine());
+            if(account.WithDraw(amount)== true){
+                account.Balance = account.Balance - amount;
+                Console.WriteLine("updated Account details are : \n");
+                account.displaydetails();
+            }
+            List<Account> accountlist = new List<Account>() {
+            new Account()  { Id = 11, AccountType = "Savings", Balance = 10000},
+            new Account()  { Id = 12, AccountType = "Savings", Balance = 12000},
+            new Account()  { Id = 13, AccountType = "Current", Balance = 8000},
+            new Account()  { Id = 14, AccountType = "Savings", Balance = 11000},
+            new Account()  { Id = 15, AccountType = "Savings", Balance = 7000},
+            new Account()  { Id = 16, AccountType = "Current", Balance = 9000},
+            new Account()  { Id = 17, AccountType = "Current", Balance = 4000},
+            new Account()  { Id = 18, AccountType = "Savings", Balance = 17000},
+            new Account()  { Id = 19, AccountType = "Savings", Balance = 7500},
+            new Account()  { Id = 20, AccountType = "Current", Balance = 9700},
+            };
+            List<int> amountlist = new List<int>() { 5000, 9000, 9000, 1000, 2000, 10000, 1000, 2000, 8000, 6000 };
+            for(int i = 0; i < amountlist.Count; i++)
+            {
+                if (accountlist[i].WithDraw(amountlist[i]) == true)
+                {
+                    accountlist[i].Balance = accountlist[i].Balance - amountlist[i];
+                    Console.WriteLine("updated Account details are : \n");
+                    accountlist[i].displaydetails();
+                }
+
+            }
+           
+        }
+    }
+}
